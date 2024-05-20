b4b232519774718a5619eb2c60ef1ca859db4e05
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-05-20 21:22:49 +0100

    added a Dockerfile to project to containerise app backend

--- app-backend/app/main.py
+++ app-backend/app/main.py
@@ -1,5 +1,5 @@
-import uvicorn
-
+# import uvicorn
+#
 # from app.api.router import router
 from api.router import router
 from fastapi import FastAPI
@@ -9,5 +9,5 @@ app = FastAPI(title="Football API", description="API for football data", version
 app.include_router(router, prefix="/api/v1")
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
