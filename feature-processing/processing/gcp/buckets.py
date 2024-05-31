032bc5569a1d1dd00e0892bec71165301181db80
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-05-31 15:02:53 +0100

    created a file that contains an attribute class with relevant bucket names

---
+++ feature-processing/processing/gcp/buckets.py
@@ -0,0 +1,5 @@
+class Buckets:
+    FBREF = "fbref_db"
+    PROCESSED_FBREF = "processed_fbref_db"
+    TRANSFERMARKT = "transfermarkt_db"
+    PROCESSED_TRANSFERMARKT = "processed_transfermarkt_db"
