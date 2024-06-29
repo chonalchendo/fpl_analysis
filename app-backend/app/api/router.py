640907b266f99ffd9c2c945d27ed904d442a0fe6
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-06-29 17:14:05 +0100

    added the new dropdown router to the main router

--- app-backend/app/api/router.py
+++ app-backend/app/api/router.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
 
-from app.api.handlers import value_prediction
+from app.api.handlers import dropdowns, value_prediction
 
 api_router = APIRouter()
 
 api_router.include_router(
     value_prediction.router,
     prefix="/value_prediction",
-    tags=["value_prediction"],
+    tags=["Value Prediction"],
+)
+
+api_router.include_router(
+    dropdowns.router,
+    prefix="/dropdowns",
+    tags=["App dropdowns"],
 )
