5a933d51f6ba8b829b85c2681b9e75f6a2c51faa
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-06-29 17:16:09 +0100

    no major changes if any

--- app-backend/app/application.py
+++ app-backend/app/application.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.router import api_router
 from app.core.config import get_settings
@@ -11,6 +12,13 @@ def get_app() -> FastAPI:
         docs_url=f"{get_settings().API_V1_STR}/docs",
         redoc_url=f"{get_settings().API_V1_STR}/redoc",
         openapi_url=f"{get_settings().API_V1_STR}/openapi.json",
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
     app.include_router(api_router, prefix=get_settings().API_V1_STR)
 
