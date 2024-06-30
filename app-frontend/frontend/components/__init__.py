ff8a8cffb73dd4686abe4ee508f26d7d03377210
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-06-30 16:26:12 +0100

    added the components to the package __init__.py file

---
+++ app-frontend/frontend/components/__init__.py
@@ -0,0 +1,5 @@
+from ._dataframe import create_dataframe
+from ._plot import create_plot
+from ._sidebar import configure_sidebar
+
+__all__ = ["configure_sidebar", "create_dataframe", "create_plot"]
